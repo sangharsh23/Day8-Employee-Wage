ab41877ce5b8398a5e7f81c29af4a087fa15edfa
sangharsh23 <sangharsh355@gmail.com>
2021-09-24 21:10:24 +0530

    Added : program part time employee and wage

--- src/com/bridgeabz/employee/wage/EmployeeWage.java
+++ src/com/bridgeabz/employee/wage/EmployeeWage.java
@@ -2,6 +2,7 @@ package com.bridgeabz.employee.wage;
 
 public class EmployeeWage {
 	public static final int Full_TIME_IS = 1;
+	public static final int PART_TIME_IS = 2;
 	public static double empCheck = 0;
 	public static int empHrs;
 	public static final int EMP_Wage_Per_Hour = 20;
@@ -10,24 +11,26 @@ public class EmployeeWage {
 		System.out.println("Welcome to the employee wage program");
 		presentOrAbsent();
 		empDailyWage();
-
 	}
 
+	// 1) check employee is present or absent
 	public static void presentOrAbsent() {
-
-		empCheck = Math.floor(Math.random() * 10) % 2;
+		empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == Full_TIME_IS) {
-			System.out.println("Employee is present");
+			System.out.println("Employee is full time present");
 			empHrs = 8;
+		} else if (empCheck == PART_TIME_IS) {
+			System.out.println("Employee is part time present");
+			empHrs = 4;
 		} else {
-			System.out.println("Employee is Absent");
-			empHrs = 0;
-			empDailyWage();
+			System.out.println("Employee is absent");
 		}
 	}
 
+	// 2) calculate employee daily Wage
 	public static void empDailyWage() {
 		int dailyWage = EMP_Wage_Per_Hour * empHrs;
 		System.out.println("Employee daily Wage : " + dailyWage);
 	}
+
 }
