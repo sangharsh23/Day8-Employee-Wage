e70a263d66aae2e5d4e69aac65237e882f84a24e
sangharsh23 <sangharsh355@gmail.com>
2021-09-24 19:36:28 +0530

    Added : program Employee present or absent

--- src/com/bridgeabz/employee/wage/EmployeeWage.java
+++ src/com/bridgeabz/employee/wage/EmployeeWage.java
@@ -4,6 +4,15 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the employee wage program");
+		presentOrAbsent();
 	}
 
+	public static void presentOrAbsent() {
+		  int fullTimeIs = 1;
+	        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+	        if (empCheck == fullTimeIs)
+	            System.out.println("Employee is present");
+	        else
+	            System.out.println("Employee is Absent");
+}
 }
