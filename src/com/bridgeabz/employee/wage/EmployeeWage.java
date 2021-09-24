142d10853295abcdd56a3ba8996491d5bd59a299
sangharsh23 <sangharsh355@gmail.com>
2021-09-24 20:52:51 +0530

    Added : program employee daily wage

--- src/com/bridgeabz/employee/wage/EmployeeWage.java
+++ src/com/bridgeabz/employee/wage/EmployeeWage.java
@@ -1,18 +1,33 @@
 package com.bridgeabz.employee.wage;
 
 public class EmployeeWage {
+	public static final int Full_TIME_IS = 1;
+	public static double empCheck = 0;
+	public static int empHrs;
+	public static final int EMP_Wage_Per_Hour = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the employee wage program");
 		presentOrAbsent();
+		empDailyWage();
+
 	}
 
 	public static void presentOrAbsent() {
-		  int fullTimeIs = 1;
-	        double empCheck = Math.floor(Math.random() * 10 ) % 2;
-	        if (empCheck == fullTimeIs)
-	            System.out.println("Employee is present");
-	        else
-	            System.out.println("Employee is Absent");
-}
+
+		empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == Full_TIME_IS) {
+			System.out.println("Employee is present");
+			empHrs = 8;
+		} else {
+			System.out.println("Employee is Absent");
+			empHrs = 0;
+			empDailyWage();
+		}
+	}
+
+	public static void empDailyWage() {
+		int dailyWage = EMP_Wage_Per_Hour * empHrs;
+		System.out.println("Employee daily Wage : " + dailyWage);
+	}
 }
