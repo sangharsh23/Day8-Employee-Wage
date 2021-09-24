b644b5888b6343e583529cf2a92487422a8e726e
sangharsh23 <sangharsh355@gmail.com>
2021-09-24 19:25:01 +0530

    Added : welcome msg

---
+++ src/com/bridgeabz/employee/wage/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgeabz.employee.wage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the employee wage program");
+	}
+
+}
